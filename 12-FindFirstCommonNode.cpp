01daa4bdd16490c79727124d6e1156959bb9e0fe
maohuui <mh_sust@163.com>
2021-01-09 22:53:39 +0800

    add 12-FindFirstCommonNode.cpp
    Signed-off-by: maohuui <mh_sust@163.com>

---
+++ 12-FindFirstCommonNode.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+struct ListNode
+{
+    int val;
+    struct ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {
+    }
+};
+
+
+/*
+输入两个链表，找出它们的第一个公共结点。（注意因为传入数据是链表，所以错误测试数据的提示是用其他方式显示的，保证传入数据是正确的）
+*/
+
+ListNode* FindFirstCommonNode( ListNode* pHead1, ListNode* pHead2) 
+{
+
+}
+
+int main()
+{
+
+    return 0;
+}
